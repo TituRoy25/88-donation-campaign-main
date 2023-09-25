ea17a5aece94e4c971f058308b6db37e94bd6fb1
TituRoy25 <tituroydinajpur11@gmail.com>
2023-09-25 14:13:19 +0600

    dynamically bg-color & text-color added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,7 +5,7 @@ const Cards = ({cards}) => {
     console.log(cards);
     return (
         <div>
-            <h1>Cards....</h1>
+            
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 py-5">
                 {
                     cards?.map(card =><CatagoryCard key={card.id} card={card}></CatagoryCard> )
